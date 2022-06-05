a9089e10fa366e547962f51c5da0f19818a89ba0
17felix <19fmysko@mail.ru>
2022-06-05 13:24:52 +0300

    add models

---
+++ src/app/models/User.ts
@@ -0,0 +1,22 @@
+export class User {
+  private readonly _id: number;
+  private _name: string;
+
+  constructor(name:string) {
+    this._id = Date.now();
+    this._name = name;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+}
